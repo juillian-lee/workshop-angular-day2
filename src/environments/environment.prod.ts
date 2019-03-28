0924857010841e5e50ee1787fcf13e6603c5cb95
juillianlee <juillianlee@livetouch.com.br>
2019-03-28 17:17:23 -0300

    ajuste de cadastro de usuario no firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBYM5vwTbWj-3Y9fP8rTirtdQ01cC1s8Ro",
+    authDomain: "workshop-angular-208e9.firebaseapp.com",
+    databaseURL: "https://workshop-angular-208e9.firebaseio.com",
+    projectId: "workshop-angular-208e9",
+    storageBucket: "workshop-angular-208e9.appspot.com",
+    messagingSenderId: "556812708499"
+  }
 };
